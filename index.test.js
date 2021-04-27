ebde03e21d85d4482cb9ab4a04203f70aa5f1aff
Mark Appleton <Mark.Appleton@ao.com>
2021-04-27 14:52:34 +0100

    added describe statements (basic)

--- index.test.js
+++ index.test.js
@@ -1,9 +1,13 @@
 const addition = require('./index.js');
 
-test('smoke test', () => {
-    expect(2+2).toBe(4);
-});
+describe('smoke tests', () => {
+    test('smoke test', () => {
+        expect(2 + 2).toBe(4);
+    })
+})
 
-test('Check that tests are working', () => {
-    expect(addition(2, 2)).toBe(4);
-});
+describe('Basic Maths', () => {
+    test('Check that tests are working', () => {
+        expect(addition(2, 2)).toBe(4);
+    })
+})
