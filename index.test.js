037af9f0e372b09a6220f8d79197efacf02bb624
Mark Appleton <Mark.Appleton@ao.com>
2021-04-27 14:47:27 +0100

    added smoke test

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
 const addition = require('./index.js');
 
+test('smoke test', () => {
+    expect(2+2).toBe(4);
+});
+
 test('Check that tests are working', () => {
     expect(addition(2, 2)).toBe(4);
 });
